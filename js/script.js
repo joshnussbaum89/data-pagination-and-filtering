4cd0038aaa77fc9e5d12627f0be3226f6b42a882
Josh Nussbaum <Joshnussbaum89@gmail.com>
2020-10-09 11:06:01 -0400

    build showPage and addPagination functions

--- js/script.js
+++ js/script.js
@@ -1,30 +1,65 @@
 /*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
-
-
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
-/*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+function showPage(list, page) {
+   const startIndex = (page * 9) - 9;
+   const endIndex = page * 9;
+   const studentList = document.querySelector('.student-list');
+   studentList.innerHTML = '';
 
+   for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+         studentList.insertAdjacentHTML("beforeend",
+            `<li class="student-item cf">
+               <div class="student-details">
+                  <img class="avatar" src=${data[i].picture.large} alt="Profile Picture">
+                  <h3>${data[i].name.title} ${data[i].name.first} ${data[i].name.last}</h3>
+                  <span class="email">${data[i].email}</span>
+               </div>
+               <div class="joined-details">
+                  <span class="date">Joined ${data[i].registered.date}</span>
+               </div>
+            </li>`);
+      }
+   }
+   return studentList;
+}
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+function addPagination(list) {
+   const numOfPaginationBtns = Math.ceil(list.length / 9);
+   const linkList = document.querySelector('.link-list');
+   linkList.innerHTML = '';
 
+   for (let i = 1; i <= numOfPaginationBtns; i++) {
+      linkList.insertAdjacentHTML("beforeend",
+         `<li>
+            <button type="button">${i}</button>
+         </li>`);
+   }
+   const firstLiItem = linkList.firstElementChild;
+   firstLiItem.classList.add('active');
+
+   linkList.addEventListener('click', (e) => {
+      if (e.target.tagName === 'BUTTON') {
+         firstLiItem.classList.remove('active');
+         e.target.classList.add('active');
+         console.log(linkList.children);
+
+         console.log(firstLiItem);
+         console.log(e.target);
+      }
+
+   })
+}
 
 // Call functions
+
+showPage(data, 1);
+addPagination(data);
